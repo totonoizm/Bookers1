84bdd2f0138c3823a737bcade8381d8eea3de5ad
EC2 Default User <ec2-user@ip-172-31-44-141.ec2.internal>
2020-10-19 08:51:17 +0000

    [add]エラーメッセージ完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,7 +16,7 @@ class BooksController < ApplicationController
     @books = Book.all
     @book = Book.new(book_params)
     if @book.save
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
       render 'new'
     end
@@ -27,8 +27,10 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @books = Book.all
+    @book = Book.new
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
      redirect_to book_path(book.id)
     else
       render 'edit'
